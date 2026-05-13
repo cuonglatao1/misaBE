fd37f855e776e171a7285d22b6cb247b52b705c4
dangduccuong <158483961+cuonglatao1@users.noreply.github.com>
2026-05-14 00:51:19 +0700

    fix: resolve namespace/type name conflicts in DL layer

--- MisaBE.DL/SalaryCompositionSystem/ISalaryCompositionSystemDL.cs
+++ MisaBE.DL/SalaryCompositionSystem/ISalaryCompositionSystemDL.cs
@@ -1,9 +1,9 @@
-using MisaBE.Common.Entities;
 using MisaBE.DL.Base;
+using SalaryCompositionSystemEntity = MisaBE.Common.Entities.SalaryCompositionSystem;
 
 namespace MisaBE.DL.SalaryCompositionSystem
 {
-    public interface ISalaryCompositionSystemDL : IBaseDL<SalaryCompositionSystem>
+    public interface ISalaryCompositionSystemDL : IBaseDL<SalaryCompositionSystemEntity>
     {
     }
 }
